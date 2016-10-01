7a04fd906f30282e6d12888ebcbd988c2b640a31
Shafqat Dulal <shafqat.dulal@gmail.com>
2016-10-01 11:49:03 -0700

    Config for messenger bot

--- app/bot/example.rb
+++ app/bot/example.rb
@@ -1,5 +1,11 @@
 include Facebook::Messenger
 
+Facebook::Messenger.configure do |config|
+  config.access_token = ENV['messenger_bot_access_token']
+  config.app_secret = ENV["SECRET_KEY_BASE"]
+  config.verify_token = ENV['messenger_bot_verify_token']
+end
+
 Bot.on :message do |message|
   Bot.deliver(
     recipient: message.sender,

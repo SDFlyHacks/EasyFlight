444a0836fefc76bbf623f8056e4aefa50694ca97
Shafqat Dulal <shafqat.dulal@gmail.com>
2016-10-02 00:13:30 -0700

    Fix the beast and provide lé schema

--- db/migrate/20161001210946_add_conversation_to_data.rb
+++
@@ -1,7 +0,0 @@
-class AddConversationToData < ActiveRecord::Migration[5.0]
-  def change
-    add_column :data, :sender, :text
-    add_column :data, :state, :integer
-    remove_column :data, :passenger_name
-  end
-end

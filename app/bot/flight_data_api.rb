440e0ff508240315cd890fc70cfd9f769f9965b1
Kevin Lee <leekevin@berkeley.edu>
2016-10-01 22:09:50 -0700

    Rename and add arrival info

--- app/bot/flight_data_api.rb
+++ app/bot/flight_data_api.rb
@@ -1,7 +1,6 @@
 require 'httparty'
 require 'nokogiri'
 
-
 def flight_data_url_generator(flight_no)
   return "http://flightaware.com/live/flight/" + flight_no
 end
@@ -20,21 +19,33 @@ def extractor(url)
   airline_link = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('.track-details').css('.track-panel-header-content').css('.track-panel-header-subhead')[0].text.split()[-1]
 
   # Departure airport
-  depart_airport, _ , depart_iata = /\(((\w|\s)+),\s\w{2}\)\s-\sK(\w{3})/.match(parse_page.css('span')[8].values[1]).captures
+  depart_city, _ , depart_iata = /\(((\w|\s)+),\s\w{2}\)\s-\sK(\w{3})/.match(parse_page.css('span')[8].values[1]).captures
 
   # Arrival airport
-  arrival_airport, _ , arrival_iata = /\(((\w|\s)+),\s\w{2}\)\s-\sK(\w{3})/.match(parse_page.css('span')[9].values[1]).captures
+  arrival_city, _ , arrival_iata = /\(((\w|\s)+),\s\w{2}\)\s-\sK(\w{3})/.match(parse_page.css('span')[9].values[1]).captures
 
   # Departing terminal
-  terminal_info = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('tbody').css('tr').css('.track-panel-departure').css('.track-panel-terminal').children().text
+  departing_terminal = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('tbody').css('tr').css('.track-panel-departure').css('.track-panel-terminal').children().text
+
+  # Arrival terminal
+  arrival_terminal = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('tbody').css('tr').css('.track-panel-arrival').css('.track-panel-terminal').children().text
 
   # Takeoff time
-  time_info = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('tbody').css('tr').css('.track-panel-scheduledtime').css('td')[0].text.strip()
-  return {:airline_link => airline_link, :depart_airport => depart_airport, :depart_iata => depart_iata, :arrival_airport => arrival_airport, :arrival_iata => arrival_iata,
-          :terminal_info => terminal_info, :time_info => time_info}
+  takeoff_time = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('tbody').css('tr').css('.track-panel-scheduledtime').css('td')[0].text.strip()
+
+  #Arrival time
+  arrival_time = parse_page.css('#slideOutPanel').css('.pageContainer').css('.layout-table').css('tbody').css('tr').css('.track-panel-scheduledtime').css('td')[1].text.strip()
+
+  return {:airline_link => airline_link, :depart_city => depart_city, :depart_iata => depart_iata,
+          :arrival_city => arrival_airport, :arrival_iata => arrival_iata,
+          :departing_terminal => departing_terminal, :arrival_terminal => arrival_terminal,
+          :takeoff_time => takeoff_time, :arrival_time => arrival_time}
 end
 
 def get_flight_data_from_number(flight_no)
   url = flight_data_url_generator(flight_no)
   return extractor(url)
 end
+
+url = flight_data_url_generator('UAL390')
+puts extractor(url)
